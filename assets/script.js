57ea89f0e32be0aaa934866e195a3cf9bdf65c7f
matthewmontiel <xoptimuszerox@gmail.com>
2023-03-09 12:33:48 -0600

    worked on more JS. still not functional

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,23 @@
-let startBtn = document.querySelector("#start-btn");
+let startBtn = document.querySelector("start");
 let timeEl = document.querySelector(".time");
 let mainEl = document.getElementById("q-card");
-let questions = document.getElementById("questions");
+let initEL = document.getElementById("init");
+let qArea = document.getElementById("q-area");
+let aArea = document.getElementById("a-area");
 let secondsLeft = 60;
 let choice1 = document.getElementById("c1");
 let choice2 = document.getElementById("c2");
 let choice3 = document.getElementById("c3");
 let choice4 = document.getElementById("c4");
+let form = document.getElementById("form");
+let choice = document.querySelectorAll(".choices");
+let feedback = document.getElementById("feedback");
+let feedbackExpiration;
+let message = document.getElementById("message");
+let initials = document.getElementById("initials");
+let inputField = document.getElementById("input");
+let submitBtn = document.getElementById("submit");
+let scoresArea = document.getElementById("scores-area");
 
 let quizQuestions = [
   {
@@ -55,6 +66,21 @@ let quizQuestions = [
   },
 ];
 
+let current = 0;
+let Q = quizQuestions[current].question;
+let C1 = quizQuestions[current].ans1;
+let C2 = quizQuestions[current].ans2;
+let C3 = quizQuestions[current].ans3;
+let C4 = quizQuestions[current].ans4;
+
+showInIt();
+
+function showInIt() {
+  initEL.classList.toggle("hide");
+}
+
+let timerInterval;
+
 function setTime() {
   let timerInterval = setInterval(function () {
     secondsLeft--;
@@ -62,9 +88,15 @@ function setTime() {
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
       sendMessage();
+      endQuiz();
     }
   }, 1000);
 }
+
+startBtn.addEventListener("click", function() {
+  setTime();
+  showQuiz();
+});
 
 function sendMessage() {
   timeEl.textContent = "0";
@@ -73,22 +105,80 @@ function sendMessage() {
   mainEl.appendChild(imgEl);
 }
 
-function startQuiz() {
-  let current = 0;
-  let Q = quizQuestions[current].question;
-  let C1 = quizQuestions[current].ans1;
-  let C2 = quizQuestions[current].ans2;
-  let C3 = quizQuestions[current].ans3;
-  let C4 = quizQuestions[current].ans4;
-
-  testArea.innerHTML = "<p>" + Q + "</p>";
-  choices1.innerHTML = C1;
-  choices2.innerHTML = C2;
-  choices3.innerHTML = C3;
-  choices4.innerHTML = C4;
+function showQuiz() {
+  timeEl.classList.toggle("hide");
+  timeEl.textContent = secondsLeft;
+  initEL.classList.toggle("hide");
+  qArea.innerHTML = Q;
+  c1btn.innerHTML = C1;
+  c2btn.innerHTML = C2;
+  c3btn.innerHTML = C3;
+  c4btn.innerHTML = C4;
 }
 
-startBtn.addEventlistener("Click", function() {
-  startQuiz();
-  setTime();
-});
+
+
+function check(event) {
+  let right = quizQuestions.answer;
+  if (event.target.textContent == right) {
+    next();
+    feedback.innerHTML = "One more Stone to add to the collection";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+  } else {
+    punish(15);
+    next();
+    feedback.innerHTML = "Your're ruining the balance";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+    stopAtZero();
+    timeEl.textContent = secondsLeft;
+  }
+}
+
+function stopAtZero() {
+  if (secondsLeft <= 0) {
+    secondsLeft = 0;
+    endQuiz();
+  }
+}
+
+function punish(seconds) {
+  secondsLeft -= seconds;
+}
+
+function hideFeedback() {
+  clearTimeout(feedbackExpiration);
+}
+
+function next() {
+  current++;
+  hideFeedback();
+
+  if (current < 5) {
+    let Q = questions[current].question;
+    let C1 = questions[current].ans1;
+    let C2 = questions[current].ans2;
+    let C3 = questions[current].ans3;
+    let c4 = questions[current].ans4;
+
+    qArea.innerHTML = Q;
+    c1btn.innerHTML = C1;
+    c2btn.innerHTML = C2;
+    c3btn.innerHTML = C3;
+    c4btn.innerHTML = C4;
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  qArea.classList.toggle("hide");
+  aArea.classList.toggle("hide");
+  form.classList.toggle("hide");
+  timeEl.classList.toggle("hide");
+}
+
